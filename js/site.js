5b1d4c53d58137be632743abbb947c290baf7f1b
Mark Miller <mark.miller2792@gmail.com>
2016-12-12 21:20:47 -0500

    Added css query plugin

--- js/site.js
+++ js/site.js
@@ -1,13 +1,21 @@
 $(document)
     .ready(function() {
-        $('#table_id')
-            .DataTable(
 
-            );
+        if ($('#table_id')
+            .length > 0) {
+            $('#table_id')
+                .DataTable(
+
+                );
+        }
+
+        if ($('.chosen-select').length > 0) {
 
 
-        $(".chosen-select")
-            .chosen({
+
+
+            $(".chosen-select").chosen({
                 disable_search_threshold: 10
             });
+        }
     });
