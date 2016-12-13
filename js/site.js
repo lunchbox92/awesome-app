f7c54cea1fe8d9f6a1f7c51a97e7cda64d0eeefc
Mark Miller <mark.miller2792@gmail.com>
2016-12-12 20:20:17 -0500

    Added bootstrap and test select box

--- js/site.js
+++ js/site.js
@@ -1,5 +1,13 @@
-$(document).ready( function () {
-    $('#table_id').DataTable(
+$(document)
+    .ready(function() {
+        $('#table_id')
+            .DataTable(
 
-    );
-} );
+            );
+
+
+        $(".chosen-select")
+            .chosen({
+                disable_search_threshold: 10
+            });
+    });
