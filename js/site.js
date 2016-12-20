4a8ec5274526536b4f5fc19f3d25b7bc9a96ecce
Mark Miller <mark.miller2792@gmail.com>
2016-12-20 18:41:32 -0500

    Added functionality to chosen using papa parse

--- js/site.js
+++ js/site.js
@@ -6,54 +6,23 @@ $(document)
                 .DataTable();
         }
 
-        if ($('.chosen-select').length > 0) {
+        var $selectList = $('.chosen-select');
 
-            $.ajax({
-                type: "GET",
-                url: "data.csv",
-                dataType: "text",
-                success: function(data) {
-                    processData(data);
+        if ($selectList.length > 0) {
+            var parsedCSV = Papa.parse('/data/hartmanpartnumbers.csv', {
+                download: true,
+                complete: function(results) {
+                    var dataArray = results.data;
+                    $.each(dataArray, function(key, value) {
+                        $selectList
+                            .append($("<option></option>")
+                                .attr("value", key)
+                                .text(value));
+                    });
+                    $selectList.chosen({
+                        disable_search_threshold: 10
+                    });
                 }
-            });
-
-            var fs = require('fs');
-            var $ = jQuery = require('jQuery');
-            require('../src/jquery.csv.js');
-
-            var sample = '/data/hartmanpartnumbers.csv';
-            $.csv.readFile(sample, 'UTF-8', function(err, csv) {
-                $.csv.toArrays(csv, {}, function(err, data) {
-                    for (var i = 0, len = data.length; i < len; i++) {
-                        console.log(data[i]);
-                    }
-                });
-            });
-
-
-            $(".chosen-select").chosen({
-                disable_search_threshold: 10
-            });
+            })
         }
     });
-
-function printTable(file) {
-    var reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = function(event) {
-        var csv = event.target.result;
-        var data = $.csv.toArrays(csv);
-        var html = '';
-        for (var row in data) {
-            html += '<tr>\r\n';
-            for (var item in data[row]) {
-                html += '<td>' + data[row][item] + '</td>\r\n';
-            }
-            html += '</tr>\r\n';
-        }
-        $('#contents').html(html);
-    };
-    reader.onerror = function() {
-        alert('Unable to read ' + file.fileName);
-    };
-}
